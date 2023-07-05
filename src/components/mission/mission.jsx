85907a4540d925db6622d0db899eaa951cb394e5
Akmaljon571 <akmaljon079@gmail.com>
2023-07-05 17:11:42 +0300

    mission bug fix

--- src/components/mission/mission.jsx
+++ src/components/mission/mission.jsx
@@ -2,38 +2,43 @@ import { useState } from 'react';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import './mission.scss'
 import { Link } from 'react-router-dom';
+import Container from '../container/container';
 
 function Mission() {
     const [open, setOpen] = useState(true);
 
-    return (  
-        <div className="mission">
-            <div className={open ? "mission-top" : "mission-top mission-top-active"}>
-                <button onClick={() => setOpen(!open)}>
-                    <span>
-                        {open ? "See Our Mission" : ''}
-                    </span>
-                    <KeyboardArrowDownIcon className={open ? 'icon' : 'icon icon-active'} />
-                </button>
-                {!open ?
-                    <>
-                    <span className='span'>
-                        Our Mission
-                    </span>
-                    <p>
-                        Supporting <span>countries worldwide </span> in their transition to a sustainable energy future
-                    </p>
-                    <Link to={'/'}>
-                        Read about us
-                    </Link>
-                    </>
-                : null}
-            </div>
-            <div className="mission-bottom">
-                <h1>168</h1>
-                <p>MEMBERS</p>
-                <span>(167 COUNTRIES & THE EU)</span>
-            </div>
+    return (
+        <div className="mission-wrapper">
+            <Container>
+                <div className="mission">
+                    <div className={open ? "mission-top" : "mission-top mission-top-active"}>
+                        <button onClick={() => setOpen(!open)}>
+                            <span>
+                                {open ? "See Our Mission" : ''}
+                            </span>
+                            <KeyboardArrowDownIcon className={open ? 'icon' : 'icon icon-active'} />
+                        </button>
+                        {!open ?
+                            <>
+                                <span className='span'>
+                                    Our Mission
+                                </span>
+                                <p>
+                                    Supporting <span>countries worldwide </span> in their transition to a sustainable energy future
+                                </p>
+                                <Link to={'/'}>
+                                    Read about us
+                                </Link>
+                            </>
+                            : null}
+                    </div>
+                    <div className="mission-bottom">
+                        <h1>168</h1>
+                        <p>MEMBERS</p>
+                        <span>(167 COUNTRIES & THE EU)</span>
+                    </div>
+                </div>
+            </Container>
         </div>
     );
 }
